01e8576e90bdfcd287abb27d7aef3e842ca67e6c
Nando Vieira <me@fnando.com>
2024-11-01 16:19:57 -0700

    Bump up version (ll v0.0.7).

--- ll.rb
+++ ll.rb
@@ -4,17 +4,17 @@ class Ll < Formula
   desc "A prettier terminal's ls command, with color and " \
        "https://www.nerdfonts.com/ icons."
   homepage "https://github.com/fnando/ll"
-  version "0.0.6"
+  version "0.0.7"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/fnando/ll/releases/download/v#{version}/ll-aarch64-apple-darwin.tar.gz"
-    sha256 "ce08553750031924c86bce1ddc8f3b4a092a9ac33f1af9cfa34835b01926a9f0"
+    sha256 "0931727623dbd2c65a22af85b30dcc86ef2e1f99ef793f431a4d5c0022daae01"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/fnando/ll/releases/download/v#{version}/ll-x86_64-apple-darwin.tar.gz"
-    sha256 "2f584c113f0594544df89c87f7722dbd37ebf90130d8f93c6d99431d61fef697"
+    sha256 "7e52f2f71bd4080199419e103e3d7eedc28dfc15d6862fd08d1d0f875045d5f8"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/fnando/ll/releases/download/v#{version}/ll-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "7245ccbd4f472daf6d7b4c911283e627379e6b6eecd6576b19eae35cb9777f59"
+    sha256 "270072807a87318d58604d7625b33d10211e0bad7c6ec94b77e16db0ec17ec7c"
   end
 
   def install
