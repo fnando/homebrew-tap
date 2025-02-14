384f56a825290792f2ee757b6e16cda84d502fc1
Nando Vieira <me@fnando.com>
2025-02-13 19:06:41 -0800

    Bump up version (notes 0.0.2)

--- notes.rb
+++ notes.rb
@@ -3,17 +3,17 @@
 class Notes < Formula
   desc "Display notes like TODO and FIXME"
   homepage "https://github.com/fnando/notes"
-  version "0.0.1"
+  version "0.0.2"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/fnando/notes/releases/download/v#{version}/notes-aarch64-apple-darwin.tar.gz"
-    sha256 "cb41e689ef9c194c6d4673b5942b35ada2ff805db4d39bbeb00e7e6b5aa5e89c"
+    sha256 "969937504ae6f1756b7361c2aae5058a104b044c3cf065d44c9a673d28f64919"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/fnando/notes/releases/download/v#{version}/notes-x86_64-apple-darwin.tar.gz"
-    sha256 "93ba93ac7a2f93486bcf82dc4af7af23216f389d54395b16e49db507677d677e"
+    sha256 "df912db6ff260899ca89c6d80d0223a666afd7595f7b5d459e2dcbc7f5e68ac4"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/fnando/notes/releases/download/v#{version}/notes-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "ba42685ab5dfd19d735ef472a513e150f6b8461f63107608cec360738b301f5a"
+    sha256 "ddb15b8060f2ee599113d45fcb4b61d2cfc3bc38dad72650b25ee9d9044b5d9c"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/fnando/notes/releases/download/v#{version}/notes-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
