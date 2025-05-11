ca9703bca8811f6610e2715d4b77fc4db47ca9fb
Nando Vieira <me@fnando.com>
2025-05-11 00:48:59 -0700

    Bump up version (op-env 0.0.4)

--- op-env.rb
+++ op-env.rb
@@ -4,17 +4,17 @@ class OpEnv < Formula
   desc "A command-line tool to retrieve secrets from 1Password, with custom " \
        "output to several formats. Depends on 1Password's own `op` command."
   homepage "https://github.com/fnando/op-env"
-  version "0.0.3"
+  version "0.0.4"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/fnando/op-env/releases/download/v#{version}/op-env-aarch64-apple-darwin.tar.gz"
-    sha256 "67d1f9d997a1f736a17b8b7743e4a63bb56261dc6728c8fbec2b50794c506d81"
+    sha256 "e73fe5684938697ba6754bfc976f5b20940c3b3d251a65818eef729adeb38b05"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/fnando/op-env/releases/download/v#{version}/op-env-x86_64-apple-darwin.tar.gz"
-    sha256 "50f643911506ab6d901087899f5c6a5080810e35893ee848fb582805f12a3583"
+    sha256 "76d2f747ffdbaeda596c95a68a89660c194ca38ae248e2924b94685ad55d26e6"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/fnando/op-env/releases/download/v#{version}/op-env-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "5430955edbc4ba8887272fd89e1fb4898e23997add4adfa8281f2cd86bbcae2e"
+    sha256 "e630454a30d1a2e707ecd94fa14ed992d5f7b80869dbddce3f9e10ed833d6e58"
   end
 
   def install
